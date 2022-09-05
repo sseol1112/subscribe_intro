8913b0fcaaa1f4194b3f1708c92f0938b191e033
sseol1112 <sseol1112@naver.com>
2022-09-05 15:47:47 +0900

    22.09.05 - scroll event 추가

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,110 +1,117 @@
 (function () {
     const subscribe = {
         settings : function() {
-            const self = this;
-            self.containerEl = document.getElementsByClassName('container');
-            self.section1 = document.querySelector('.sec1');
-            self.section2 = document.getElementsByClassName('sec2');
-            self.section3 = document.querySelector('.sec3');
-            self.section4 = document.querySelector('.sec4');
-            self.section5 = document.querySelector('.sec5');
-            self.section6 = document.querySelector('.sec6');
-            self.section7 = document.querySelector('.sec7');
+            const self = this;            
+
+            self.$containerEl = $('.container');
+            self.$section1 = self.$containerEl.find('.sec1');
+            self.$section2 = self.$containerEl.find('.sec2');
+            self.$section3 = self.$containerEl.find('.sec3');
+            self.$section4 = self.$containerEl.find('.sec4');
+            self.$section5 = self.$containerEl.find('.sec5');
+            self.$allDiv = $('section div');
+            self.$allImg = $('section img');
+
 
             self.sec1Text = document.querySelector('.sec1 .text_area');
             self.sec1Img = document.querySelectorAll('.sec1 .img_area img');
             self.sec2Img = document.querySelector('.sec2 .img_area img.ico_02');
             self.sec3Img = document.querySelectorAll('.sec3 .img_area img');
             self.sec4Img = document.querySelectorAll('.sec4 .img_area img');
+            self.sec5Img = document.querySelector('.sec5 .sec_icon img');
 
-            self.$scrollTop = $(window).scrollTop();
         },
         init : function() {
             const self = this;
-            console.log('init');
+            //console.log('init');
             self.settings();
             self.bindEvents();
 
 
-            setTimeout(function() { 
-                // $('.sec1 .text_area').addClass("on");
+            setTimeout(function() {
                 self.sec1Text.classList.add('on');
 
                 for(let i=0; i<self.sec1Img.length; i++) {
                     self.sec1Img[i].classList.add('on');
                 }
+                
+                // self.sec2Img.classList.add('on');
 
-                self.sec2Img.classList.add('on');
+                // if(self.sec2Img.classList.contains('on')){
+                //     self.sec2Img.setAttribute('src','/images/sec2_prod_02_on.png')
+                // }
 
-                for(let i=0; i<self.sec3Img.length; i++) {
-                    self.sec3Img[i].classList.add('on');
-                }
-                for(let i=0; i<self.sec4Img.length; i++) {
-                    self.sec4Img[i].classList.add('on');
-                }
+                // for(let i=0; i<self.sec3Img.length; i++) {
+                //     self.sec3Img[i].classList.add('on');
+                // }
+                // for(let i=0; i<self.sec4Img.length; i++) {
+                //     self.sec4Img[i].classList.add('on');
+                // }
 
-                if(self.sec2Img.classList.contains('on')){
-                    self.sec2Img.setAttribute('src','/images/sec2_prod_02_on.png')
-                }
+                // self.sec5Img.classList.add('on');
             }, 1000);
-
-
-            window.addEventListener('scroll' ,function(){
-                self.scrollChk();
-            });
         },
         bindEvents : function() {
             const self = this;
             console.log('bind events');
             
-        },
-        scrollChk : function() {            
-            const self = this;
+            //스크롤 다운시 하단 이벤트 발생!
+            window.addEventListener('scroll', function(){
+                self.sec2Img.setAttribute('src','/images/sec2_prod_02_mo.png')
+                self.$allDiv.removeClass("on");
+                self.$allImg.removeClass("on");
 
 
-            var scrollTopH = $(window).scrollTop();
-            var sectionH = $('section').outerHeight();
-            var currH = scrollTopH - sectionH;
-
-            self.sec1Text.classList.remove('on');
-            for(let i=0; i<self.sec1Img.length; i++) {
-                self.sec1Img[i].classList.remove('on');
-            }
-            
-            self.sec2Img.classList.remove('on');
-            self.sec2Img.setAttribute('src','/images/sec2_prod_02_mo.png')
-
-            self.sec1Text.classList.remove('on');
-            for(let i=0; i<self.sec3Img.length; i++) {
-                self.sec3Img[i].classList.remove('on');
-            }
-
-            self.sec1Text.classList.remove('on');
-            for(let i=0; i<self.sec4Img.length; i++) {
-                self.sec4Img[i].classList.remove('on');
-            }
+                const sec1T = self.$section1.offset().top,
+                      sec1H = self.$section1.outerHeight(),
+                      sec2T = self.$section2.offset().top,
+                      sec2H = self.$section2.outerHeight(),
+                      sec3T = self.$section3.offset().top,
+                      sec3H = self.$section3.outerHeight(),
+                      sec4T = self.$section4.offset().top,
+                      sec4H = self.$section4.outerHeight(),
+                      sec5T = self.$section5.offset().top,
+                      sec5H = self.$section5.outerHeight(),
+                      wH = $(window).height(),
+                      wS = $(this).scrollTop();
 
 
-            if(currH){
-                $(this).closest('section').css('background','blue');
+                if (wS > (sec1T+sec1H-wH)){
+                    self.sec1Text.classList.add('on');
 
-                self.sec1Text.classList.add('on');
-                for(let i=0; i<self.sec1Img.length; i++) {
-                    self.sec1Img[i].classList.add('on');
-                }
+                    for(let i=0; i<self.sec1Img.length; i++) {
+                        self.sec1Img[i].classList.add('on');
+                    }
+                    //console.log('섹션1 실행')
+                } 
+
+                if (wS > (sec2T+sec2H-wH)){
+                    self.sec2Img.classList.add('on');
+                    if(self.sec2Img.classList.contains('on')){
+                        self.sec2Img.setAttribute('src','/images/sec2_prod_02_on.png')
+                    }
+                    //console.log('섹션2 실행')
+                } 
                 
-                self.sec2Img.classList.add('on');
-                self.sec2Img.setAttribute('src','/images/sec2_prod_02_on.png')
-
-                for(let i=0; i<self.sec3Img.length; i++) {
-                    self.sec3Img[i].classList.add('on');
+                
+                if (wS > (sec3T+sec3H-wH)) {
+                    for(let i=0; i<self.sec3Img.length; i++) {
+                        self.sec3Img[i].classList.add('on');
+                    }
+                    //console.log('섹션3 실행')
                 }
-                for(let i=0; i<self.sec4Img.length; i++) {
-                    self.sec4Img[i].classList.add('on');
+                if (wS > (sec4T+sec4H-wH)) {
+                    for(let i=0; i<self.sec4Img.length; i++) {
+                        self.sec4Img[i].classList.add('on');
+                    }
+                    //console.log('섹션4 실행')
                 }
-            }
 
-        }
+                if (wS > (sec5T+sec5H-wH)) {
+                    self.sec5Img.classList.add('on');
+                }
+            })
+        },
     }
     subscribe.init();
 }());
