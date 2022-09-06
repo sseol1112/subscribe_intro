e87cb82f7ee22cf15c6364f901d16d5b33962cff
sseol1112 <sseol1112@naver.com>
2022-09-06 21:35:03 +0900

    22.09.06 스크립트, css수정 - 스크립트 스크롤 이벤트 처리 소스 수정 - css transition 속도 변경

--- js/subscribe.js
+++ js/subscribe.js
@@ -8,17 +8,13 @@
             self.$section2 = self.$containerEl.find('.sec2');
             self.$section3 = self.$containerEl.find('.sec3');
             self.$section4 = self.$containerEl.find('.sec4');
-            self.$section5 = self.$containerEl.find('.sec5');
             self.$allDiv = $('section div');
             self.$allImg = $('section img');
 
-
-            self.sec1Text = document.querySelector('.sec1 .text_area');
             self.sec1Img = document.querySelectorAll('.sec1 .img_area img');
             self.sec2Img = document.querySelector('.sec2 .img_area img.ico_02');
             self.sec3Img = document.querySelectorAll('.sec3 .img_area img');
             self.sec4Img = document.querySelectorAll('.sec4 .img_area img');
-            self.sec5Img = document.querySelector('.sec5 .sec_icon img');
 
         },
         init : function() {
@@ -29,7 +25,6 @@
 
 
             setTimeout(function() {
-                self.sec1Text.classList.add('on');
 
                 for(let i=0; i<self.sec1Img.length; i++) {
                     self.sec1Img[i].classList.add('on');
@@ -47,16 +42,15 @@
                 // for(let i=0; i<self.sec4Img.length; i++) {
                 //     self.sec4Img[i].classList.add('on');
                 // }
-
-                // self.sec5Img.classList.add('on');
             }, 1000);
         },
         bindEvents : function() {
             const self = this;
-            console.log('bind events');
+            console.log('bind events');        
             
             //스크롤 다운시 하단 이벤트 발생!
-            window.addEventListener('scroll', function(){
+            window.addEventListener('scroll',function(){
+                            
                 self.sec2Img.setAttribute('src','/images/sec2_prod_02_mo.png')
                 self.$allDiv.removeClass("on");
                 self.$allImg.removeClass("on");
@@ -70,14 +64,13 @@
                       sec3H = self.$section3.outerHeight(),
                       sec4T = self.$section4.offset().top,
                       sec4H = self.$section4.outerHeight(),
-                      sec5T = self.$section5.offset().top,
-                      sec5H = self.$section5.outerHeight(),
+                    //   sec5T = self.$section5.offset().top,
+                    //   sec5H = self.$section5.outerHeight(),
                       wH = $(window).height(),
                       wS = $(this).scrollTop();
 
 
                 if (wS > (sec1T+sec1H-wH) && wS < (sec2T+sec2H-wH)){
-                    self.sec1Text.classList.add('on');
 
                     for(let i=0; i<self.sec1Img.length; i++) {
                         self.sec1Img[i].classList.add('on');
@@ -99,14 +92,20 @@
                         self.sec4Img[i].classList.add('on');
                     }
                     console.log('섹션4 실행')
-                } else if(wS >= 1900) {
-                    self.sec5Img.classList.add('on');
-                    console.log('섹션5 실행');
                 } else {
                     console.log("Error!, 잘못된 형식입니다.");
                 }
+                
+                
             })
-        },
+            // var dh = $(document).height();
+            // window.addEventListener('scroll', function(){
+            //     var st = $(this).scrollTop();                
+            //     var bh = $(window).height();
+
+            //     console.log(st);
+            // })
+        }
     }
     subscribe.init();
 }());
